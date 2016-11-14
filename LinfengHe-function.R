bb46cace1fad79703197ee25543867fc714e5203
Hope He <lh543@scarletmail.rutgers.edu>
2016-11-14 01:06:51 -0500

    second commit simplying the extrating process

--- LinfengHe-function.R
+++ LinfengHe-function.R
@@ -2,13 +2,8 @@ clean_who_number_spaces <- function(x, sep = ""){
   ## Change formating of numbers from xxx xxx to xxxxxx
   library(stringr)
   library(dplyr)
-  clean_triplet_pair <- 
-    function(x) str_replace(x, "([0-9]{1,3}) ([0-9]{3})", paste0("\\1\\2"))
-  number_of_iterations <- max(map_int(x, ~ str_count(., " ")), na.rm = TRUE)
-  for (i in 1:number_of_iterations){
-    x <- clean_triplet_pair(x)
-  }
-  x
+  x <- str_replace_all(x, " ", "")
+  return(x)
 }
 
 who_pull_middle <- function(x){
